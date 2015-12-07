2ad7aa1edd7bb6a785edb4c1cf447d53225672e2
tlariviere <lariviere.thibaud@gmail.com>
2015-12-07 18:18:28 +0100

    Create Matrix.hxx

---
+++ Matrix.hxx
@@ -0,0 +1,32 @@
+
+#ifndef MATRIX_HXX
+#define MATRIX_HXX
+
+#include "linalg/Linalg.hxx"
+#include <iostream>
+
+using namespace std;
+using namespace linalg;
+
+class Matrix
+{
+	public:
+		Matrix();
+		Matrix(const Matrix& m);
+		Matrix(int _N);
+		int GetM() const;
+		void Zero();
+		void Reallocate(int _N);
+		void Resize(int _N);
+		double& operator()(int i, int j);
+		const double& operator()(int i, int j) const;
+		Matrix& operator=(const Matrix& m);
+	protected:
+		int N;
+		Vector<Vector<double> > val;
+};
+
+ostream& operator<<(ostream& out, const Matrix& m);
+
+
+#endif
