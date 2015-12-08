f1c1724afddab496866d2349c083ef95a74716a5
labarraud <labarraud@enseirb-matmeca.fr>
2015-12-08 10:41:36 +0100

    Creation of DiffusionConvectionProblem.hxx

---
+++ DiffusionConvectionProblem.hxx
@@ -0,0 +1,26 @@
+#ifndef DIFFUSION_CONVECTION_PROBLEM_HXX
+
+#include "TimeSchema.hxx"
+
+
+class DiffusionConvectionProblem : public VirtualOdeSystem
+{
+
+protected:
+	Vector<double> step_x, step_y;
+	Matrix VX,VY;
+	double D,delta_x,delta_t,delta_t;
+
+
+public:
+//DiffusionConvectionProblem();
+	void Init(double delta_x, delta_t,double D);
+	void ChampsCirculaire(double center, double intensite);
+	void AddFunction(double alpha, const Matrix& rho, double t, Matrix& y);
+	void WriteGnuPlot(Matrix M,const string& nom);
+
+};
+
+
+#define DIFFUSION_CONVECTION_PROBLEM_HXX
+#endif
